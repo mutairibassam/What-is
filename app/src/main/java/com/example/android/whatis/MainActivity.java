671ca90a197016cfeb1f6d0974d5025fd5dfb475
BassamMutairi <mutairibassam@gmail.com>
2020-06-15 01:24:39 +0300

    Back-end connection

--- app/src/main/java/com/example/android/whatis/MainActivity.java
+++ app/src/main/java/com/example/android/whatis/MainActivity.java
@@ -1,14 +1,75 @@
 package com.example.android.whatis;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.Query;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
+
+    private EditText userInput;
+    private TextView searchResult;
+    private Button btnSearch;
+
+    private String storedUserInput;
+
+    private FirebaseFirestore firebaseFirestore = FirebaseFirestore.getInstance();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        userInput = findViewById(R.id.et_whatis);
+        searchResult = findViewById(R.id.tv_answer);
+        btnSearch = findViewById(R.id.btn_search);
+
+        btnSearch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                storedUserInput = userInput.getText().toString();
+
+                firebaseFirestore.collection("keywords")
+                        .get()
+                        .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                            @Override
+                            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                                if(task.isSuccessful()) {
+                                    for(QueryDocumentSnapshot documentSnapshot : task.getResult()) {
+                                        Log.d(TAG, "onComplete: " + documentSnapshot.getId() + " " + documentSnapshot.getData());
+                                        String value = documentSnapshot.getId();
+                                        if(value.equals(storedUserInput)) {
+                                            searchResult.setText(documentSnapshot.getData().toString());
+                                            return;
+                                        } else {
+                                            Toast.makeText(getApplicationContext(), "Not found", Toast.LENGTH_LONG).show();
+                                        }
+                                    }
+                                } else {
+                                    Log.w(TAG, "Error getting documents", task.getException() );
+                                }
+                            }
+                        });
+            }
+        });
+
     }
 }
